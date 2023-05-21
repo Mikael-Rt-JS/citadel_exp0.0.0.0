3695216abba0eb966e30d835dfec38e4f5b037ad
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2023-05-21 11:52:04 +0400

    Update index.js
    new index

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const fs=require('fs')
 let {l,main,PORT} =require('./citadel/server')
 let citadelControl=require('./citadel/citadelControl')
+let {MainMind}=require('./MainMind.js');
 
-//let {Index}=require('./citadel/classes/Index.js');
-let {PagesMaker}=require('./citadel/classes/PagesMaker.js');
-
-//let home=new Index(main,citadelControl);
-let pages_maker=new PagesMaker(main,citadelControl,fs);
-
+let mainMind=new MainMind(main,citadelControl,fs);
 
 /*запуск сервера*/
 main.listen(PORT,()=>{
@@ -24,3 +20,17 @@ main.listen(PORT,()=>{
 //https://www.youtube.com/watch?v=hO6V48GvmSI
 //отключить порт командой taskkill /pid 18264 /f
 //https://levelup.gitconnected.com/how-to-kill-server-when-seeing-eaddrinuse-address-already-in-use-16c4c4d7fe5d
+
+// let {Index}=require('./citadel/classes/Index.js');
+// let {Basket}=require('./citadel/classes/Basket.js');
+// let {News}=require('./citadel/classes/News.js');
+// let {Product}=require('./citadel/classes/Product.js');
+// let {Signin}=require('./citadel/classes/Signin.js');
+// let {Signup}=require('./citadel/classes/Signup.js');
+
+// let home=new Index(main,citadelControl);
+// let basket=new Basket(main,citadelControl);
+// let product=new Product(main,citadelControl);
+// let news=new News(main,citadelControl);
+// let signin=new Signin(main,citadelControl);
+// let signup=new Signup(main,citadelControl);
